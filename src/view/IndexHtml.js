c65b8f619d2f1bd54ed38aeb3922f2499ac51d5b
Christoph Pickl <christoph.pickl@gmail.com>
2020-05-22 20:43:59 +0200

    delete them afterwards

--- src/view/IndexHtml.js
+++ src/view/IndexHtml.js
@@ -3,41 +3,66 @@ exports.__esModule = true;
 var IndexHtml = /** @class */ (function () {
     function IndexHtml() {
     }
+    // CRUD
+    // ================================================================================================================
     IndexHtml.btnCreate = function () {
-        return document.getElementById(IndexHtml.ID_BTN_CREATE);
+        return document.getElementById("btnCreate");
     };
     IndexHtml.btnUpdate = function () {
-        return document.getElementById(IndexHtml.ID_BTN_UPDATE);
+        return document.getElementById("btnUpdate");
     };
     IndexHtml.btnCancel = function () {
-        return document.getElementById(IndexHtml.ID_BTN_CANCEL);
+        return document.getElementById("btnCancel");
     };
     IndexHtml.btnDelete = function () {
-        return document.getElementById(IndexHtml.ID_BTN_DELETE);
+        return document.getElementById("btnDelete");
+    };
+    IndexHtml.inpId = function () {
+        return document.getElementById("inpId");
+    };
+    IndexHtml.inpTitle = function () {
+        return document.getElementById("inpTitle");
+    };
+    IndexHtml.inpTags = function () {
+        return document.getElementById("inpTags");
+    };
+    IndexHtml.inpBody = function () {
+        return document.getElementById("inpBody");
+    };
+    IndexHtml.inpCreated = function () {
+        return document.getElementById("inpCreated");
+    };
+    IndexHtml.inpUpdated = function () {
+        return document.getElementById("inpUpdated");
+    };
+    IndexHtml.inpLikes = function () {
+        return document.getElementById("inpLikes");
+    };
+    // SEARCH
+    // ================================================================================================================
+    IndexHtml.inpSearch = function () {
+        return document.getElementById("inpSearch");
+    };
+    IndexHtml.onInpSearchInput = function (action) {
+        IndexHtml.inpSearch().addEventListener("input", action);
     };
     IndexHtml.btnCancelSearch = function () {
-        return document.getElementById(IndexHtml.ID_BTN_CANCEL_SEARCH);
+        return document.getElementById("btnCancelSearch");
     };
     IndexHtml.btnCancelSearchVisible = function (isVisible) {
         IndexHtml.btnCancelSearch().hidden = !isVisible;
     };
-    IndexHtml.onInpSearchInput = function (action) {
-        document.getElementById("inpSearch").addEventListener("input", action);
+    IndexHtml.articleList = function () {
+        return document.getElementById(IndexHtml.ID_ARTICLE_LIST);
     };
     // COMMON
     // ================================================================================================================
     IndexHtml.onClick = function (element, action) {
         element.addEventListener("click", function () { action(); });
     };
-    // CRUD
+    // MAIN
     // ================================================================================================================
-    IndexHtml.ID_BTN_CREATE = "btnCreate";
-    IndexHtml.ID_BTN_UPDATE = "btnUpdate";
-    IndexHtml.ID_BTN_CANCEL = "btnCancel";
-    IndexHtml.ID_BTN_DELETE = "btnDelete";
-    // SEARCH
-    // ================================================================================================================
-    IndexHtml.ID_BTN_CANCEL_SEARCH = "btnCancelSearch";
+    IndexHtml.ID_ARTICLE_LIST = "articleList";
     return IndexHtml;
 }());
 exports["default"] = IndexHtml;
--- src/view/UiHandler.js
+++ src/view/UiHandler.js
@@ -1,10 +1,12 @@
 "use strict";
 exports.__esModule = true;
-var common_1 = require("../common");
+var domain_1 = require("../domain");
 var IndexHtml_1 = require("./IndexHtml");
+var events_1 = require("./events");
 var UiHandler = /** @class */ (function () {
-    function UiHandler(articleService) {
+    function UiHandler(articleService, eventBus) {
         this.articleService = articleService;
+        this.eventBus = eventBus;
     }
     UiHandler.prototype.init = function () {
         var _this = this;
@@ -16,74 +18,62 @@ var UiHandler = /** @class */ (function () {
             }
         });
         IndexHtml_1["default"].btnCancelSearchVisible(false);
-        IndexHtml_1["default"].onClick(IndexHtml_1["default"].btnCreate(), function () { _this.onCreateClicked(); });
-        IndexHtml_1["default"].onClick(IndexHtml_1["default"].btnUpdate(), function () { _this.onUpdateClicked(); });
-        IndexHtml_1["default"].onClick(IndexHtml_1["default"].btnCancel(), function () { _this.onCancelClicked(); });
-        IndexHtml_1["default"].onClick(IndexHtml_1["default"].btnDelete(), function () { _this.onDeleteClicked(); });
+        IndexHtml_1["default"].onClick(IndexHtml_1["default"].btnCreate(), function () {
+            _this.eventBus.dispatch(new events_1.CreateEvent());
+        });
+        IndexHtml_1["default"].onClick(IndexHtml_1["default"].btnUpdate(), function () {
+            _this.eventBus.dispatch(new events_1.UpdateEvent());
+        });
+        IndexHtml_1["default"].onClick(IndexHtml_1["default"].btnCancel(), function () {
+            _this.onCancelClicked();
+        });
+        IndexHtml_1["default"].onClick(IndexHtml_1["default"].btnDelete(), function () {
+            _this.eventBus.dispatch(new events_1.DeleteEvent());
+        });
         this.registerSearchListener();
-        this.switchButtonsToCreateMode(true);
+        UiHandler.switchButtonsToCreateMode(true);
         this.removeAndPrependArticleNodes(this.articleService.loadArticles());
-    };
-    UiHandler.prototype.onCreateClicked = function () {
-        console.log("onCreateClicked()");
-        var article = this.readArticleFromUI(common_1.randomUuid());
-        var now = new Date();
-        article.created = now;
-        article.updated = now;
-        article.likes = 0;
-        if (!this.validateArticle(article)) {
-            return;
-        }
-        this.removeAndPrependArticleNodes(this.articleService.saveArticle(article));
-        this.resetInputs();
-    };
-    UiHandler.prototype.onUpdateClicked = function () {
-        console.log("onUpdateClicked()");
-        var article = this.readArticleFromUI();
-        if (!this.validateArticle(article)) {
-            return;
-        }
-        article.updated = new Date();
-        this.setInputValue("inpUpdated", JSON.stringify(article.updated).split("\"").join("")); // pseudo replaceAll :-/
-        this.removeAndPrependArticleNodes(this.articleService.updateArticle(article));
     };
     UiHandler.prototype.onCancelClicked = function () {
         console.log("onCancelClicked()");
-        this.resetInputs();
-    };
-    UiHandler.prototype.onDeleteClicked = function () {
-        console.log("onDeleteClicked()");
-        this.removeAndPrependArticleNodes(this.articleService.deleteArticle(this.getInputValue("inpId")));
-        this.resetInputs();
+        this.resetArticleForm();
     };
     UiHandler.prototype.onArticleTitleClicked = function (article) {
         this.scrollToTop();
         this.updateArticleForm(article);
     };
-    UiHandler.prototype.onArticleTagClicked = function (tag) {
-        var oldSearch = this.getInputValue("inpSearch");
-        var tagHashed = "#" + tag;
-        var newSearch = (oldSearch.length == 0) ? tagHashed : oldSearch + " " + tagHashed;
-        this.setInputValue("inpSearch", newSearch);
+    UiHandler.prototype.onArticleTagClicked = function (clickedTag) {
+        var oldSearch = IndexHtml_1["default"].inpSearch().value;
+        var tagHashed = "#" + clickedTag;
+        // TODO UI: mit SHIFT+click auf tag: reset search to only this tag
+        IndexHtml_1["default"].inpSearch().value = (oldSearch.length == 0) ? tagHashed : oldSearch + " " + tagHashed;
         this.onSearchInput();
     };
     // SEARCH
     // ------------========================================================------------
     UiHandler.prototype.registerSearchListener = function () {
         var _this = this;
-        IndexHtml_1["default"].onInpSearchInput(function () { _this.onSearchInput(); });
-        document.getElementById("inpSearch").addEventListener("input", function () { _this.onSearchInput(); });
-        document.getElementById("inpSearch").addEventListener('keydown', function (event) {
+        IndexHtml_1["default"].onInpSearchInput(function () {
+            _this.onSearchInput();
+        });
+        IndexHtml_1["default"].inpSearch().addEventListener("input", function () {
+            _this.onSearchInput();
+        });
+        IndexHtml_1["default"].inpSearch().addEventListener('keydown', function (event) {
             var key = event.key; // Or const {key} = event; in ES6+
             if (key === "Escape") {
                 _this.resetSearch();
             }
         });
-        IndexHtml_1["default"].onClick(IndexHtml_1["default"].btnCancelSearch(), function () { _this.resetSearch(); });
+        IndexHtml_1["default"].onClick(IndexHtml_1["default"].btnCancelSearch(), function () {
+            _this.resetSearch();
+        });
     };
     UiHandler.prototype.onSearchInput = function () {
-        var searchTerm = this.getInputValue("inpSearch").trim();
-        var terms = searchTerm.split(" ").filter(function (it) { return it.length != 0; });
+        var searchTerm = IndexHtml_1["default"].inpSearch().value.trim();
+        var terms = searchTerm.split(" ").filter(function (it) {
+            return it.length != 0;
+        });
         console.log("onSearchInput(" + searchTerm + ") => terms:", terms);
         if (terms.length == 0) {
             this.resetSearch();
@@ -94,52 +84,52 @@ var UiHandler = /** @class */ (function () {
         document.getElementById("btnCancelSearch").hidden = false;
     };
     UiHandler.prototype.resetSearch = function () {
-        this.setInputValue("inpSearch", "");
+        IndexHtml_1["default"].inpSearch().value = "";
         this.removeAndPrependArticleNodes(this.articleService.disableSearch());
         document.getElementById("btnCancelSearch").hidden = true;
     };
     // UI LOGIC
     // ------------========================================================------------
+    UiHandler.prototype.writeArticleUpdatedInputValue = function (updated) {
+        IndexHtml_1["default"].inpUpdated().value = updated;
+    };
     UiHandler.prototype.readArticleFromUI = function (givenId) {
         if (givenId === void 0) { givenId = undefined; }
-        return {
-            id: (givenId !== undefined) ? givenId : this.getInputValue("inpId"),
-            title: this.getInputValue("inpTitle"),
-            tags: this.getInputValue("inpTags").split(" ").filter(function (it) { return it.length > 0; }),
-            body: this.getInputValue("inpBody"),
-            created: new Date(this.getInputValue("inpCreated")),
-            updated: new Date(this.getInputValue("inpUpdated")),
-            likes: parseInt(this.getInputValue("inpLikes"))
-        };
+        return new domain_1.Article(
+        /*id*/ (givenId !== undefined) ? givenId : IndexHtml_1["default"].inpId().value, 
+        /*title*/ IndexHtml_1["default"].inpTitle().value, 
+        /*tags*/ IndexHtml_1["default"].inpTags().value.split(" ").filter(function (it) {
+            return it.length > 0;
+        }), 
+        /*body*/ IndexHtml_1["default"].inpBody().value, 
+        /*created*/ new Date(IndexHtml_1["default"].inpCreated().value), 
+        /*updated*/ new Date(IndexHtml_1["default"].btnUpdate().value), 
+        /*likes*/ parseInt(IndexHtml_1["default"].inpLikes().value));
     };
     UiHandler.prototype.updateArticleForm = function (article) {
-        this.setInputValue("inpId", article.id);
-        this.setInputValue("inpTitle", article.title);
-        this.setInputValue("inpTags", article.tags.join(" "));
-        this.setInputValue("inpBody", article.body);
-        this.setInputValue("inpCreated", article.created.toString());
-        this.setInputValue("inpUpdated", article.updated.toString());
-        this.setInputValue("inpLikes", article.likes.toString());
-        this.switchButtonsToCreateMode(false);
+        IndexHtml_1["default"].inpId().value = article.id;
+        IndexHtml_1["default"].inpTitle().value = article.title;
+        IndexHtml_1["default"].inpTags().value = article.tags.join(" ");
+        IndexHtml_1["default"].inpBody().value = article.body;
+        IndexHtml_1["default"].inpCreated().value = article.created.toString();
+        IndexHtml_1["default"].inpUpdated().value = article.updated.toString();
+        IndexHtml_1["default"].inpLikes().value = article.likes.toString();
+        UiHandler.switchButtonsToCreateMode(false);
     };
-    UiHandler.prototype.resetInputs = function () {
-        this.setInputValue("inpId", "");
-        this.setInputValue("inpTitle", "");
-        this.setInputValue("inpTags", "");
-        this.setInputValue("inpBody", "");
-        this.setInputValue("inpCreated", "");
-        this.setInputValue("inpUpdated", "");
-        this.setInputValue("inpLikes", "");
-        this.switchButtonsToCreateMode(true);
+    UiHandler.prototype.resetArticleForm = function () {
+        IndexHtml_1["default"].inpId().value = "";
+        IndexHtml_1["default"].inpTitle().value = "";
+        IndexHtml_1["default"].inpTags().value = "";
+        IndexHtml_1["default"].inpBody().value = "";
+        IndexHtml_1["default"].inpCreated().value = "";
+        IndexHtml_1["default"].inpUpdated().value = "";
+        IndexHtml_1["default"].inpLikes().value = "";
+        UiHandler.switchButtonsToCreateMode(true);
     };
-    // resetArticleList() {
-    //     let articles = this.articleService.loadArticles();
-    //     this.removeAndPrependArticleNodes(articles);
-    // }
     UiHandler.prototype.removeAndPrependArticleNodes = function (articles) {
         var _this = this;
-        var articleList = document.getElementById("articleList");
-        UiHandler.removeAll(articleList);
+        var articleList = IndexHtml_1["default"].articleList();
+        UiHandler.removeAllChildren(articleList);
         articles.forEach(function (article) {
             articleList.prepend(_this.createArticleNode(article));
         });
@@ -150,19 +140,22 @@ var UiHandler = /** @class */ (function () {
         var allTagsCounted = this.countEachTag(articles);
         var sortedTags = Array.from(allTagsCounted.keys()).sort();
         var tagsSummaryNode = document.getElementById("tagsSummary");
-        UiHandler.removeAll(tagsSummaryNode);
+        UiHandler.removeAllChildren(tagsSummaryNode);
         var listNode = document.createElement("ul");
         sortedTags.forEach(function (tagName) {
             var aNode = document.createElement("a");
             aNode.innerText = "#" + tagName + "(" + allTagsCounted.get(tagName) + ")";
             aNode.href = "#";
-            aNode.onclick = function () { _this.onArticleTagClicked(tagName); };
+            aNode.onclick = function () {
+                _this.onArticleTagClicked(tagName);
+            };
             var itemNode = document.createElement("li");
             itemNode.appendChild(aNode);
             listNode.appendChild(itemNode);
         });
         tagsSummaryNode.appendChild(listNode);
     };
+    // TODO outsource
     UiHandler.prototype.countEachTag = function (articles) {
         var allTagsCounted = new Map();
         articles.forEach(function (article) {
@@ -177,68 +170,106 @@ var UiHandler = /** @class */ (function () {
         return allTagsCounted;
     };
     /** general utility method / extension function */
-    UiHandler.removeAll = function (node) {
+    UiHandler.removeAllChildren = function (node) {
         while (node.firstChild) {
             node.removeChild(node.lastChild);
         }
     };
-    // MISC
-    // ------------========================================================------------
-    // TODO move inside article
-    UiHandler.prototype.validateArticle = function (article) {
-        if (article.title.trim().length == 0) {
-            alert("title must not be empty!");
-            return false;
-        }
-        return true;
+    UiHandler.prototype.addArticle = function (article) {
+        IndexHtml_1["default"].articleList().prepend(this.createArticleNode(article));
+    };
+    UiHandler.prototype.deleteArticle = function (id) {
+        var child = UiHandler.findArticleChildNodeById(id);
+        IndexHtml_1["default"].articleList().removeChild(child);
     };
     UiHandler.prototype.createArticleNode = function (article) {
         var _this = this;
         var articleTitle = document.createElement("h1");
-        articleTitle.classList.add("articleTitle");
+        articleTitle.classList.add(UiHandler.CLASS_TITLE);
         var articleTitleLink = document.createElement("a");
         articleTitleLink.innerText = article.title;
         articleTitleLink.href = "#";
-        articleTitleLink.onclick = function () { _this.onArticleTitleClicked(article); };
+        articleTitleLink.onclick = function () {
+            _this.onArticleTitleClicked(article);
+        };
         articleTitle.appendChild(articleTitleLink);
         var articleTags = document.createElement("p");
-        articleTags.classList.add("articleTags");
-        article.tags.forEach(function (tag) {
-            var tagNode = document.createElement("a");
-            tagNode.classList.add("clickableTag");
-            tagNode.innerText = "#" + tag;
-            tagNode.href = "#";
-            tagNode.onclick = function () { _this.onArticleTagClicked(tag); };
-            articleTags.appendChild(tagNode);
-        });
-        var articleBody = document.createElement("p");
-        articleBody.classList.add("articleBody");
+        articleTags.classList.add(UiHandler.CLASS_TAGS);
+        this.resetTags(articleTags, article.tags);
+        var articleBody = document.createElement("pre");
+        articleBody.classList.add(UiHandler.CLASS_BODY);
         articleBody.innerText = article.body;
         var articleNode = document.createElement("div");
+        articleNode.setAttribute(UiHandler.ATTR_ARTIFACT_ID, article.id);
         articleNode.classList.add("articleNode");
         articleNode.appendChild(articleTitle);
         articleNode.appendChild(articleTags);
         articleNode.appendChild(articleBody);
         return articleNode;
     };
-    UiHandler.prototype.switchButtonsToCreateMode = function (isCreateMode) {
-        IndexHtml_1["default"].btnCreate().hidden = !isCreateMode;
-        document.getElementById("btnUpdate").hidden = isCreateMode;
-        document.getElementById("btnCancel").hidden = isCreateMode;
-        document.getElementById("btnDelete").hidden = isCreateMode;
+    UiHandler.prototype.resetTags = function (html, tags) {
+        var _this = this;
+        UiHandler.removeAllChildren(html);
+        tags.forEach(function (tag) {
+            var tagNode = document.createElement("a");
+            tagNode.classList.add("clickableTag");
+            tagNode.innerText = "#" + tag;
+            tagNode.href = "#";
+            tagNode.onclick = function () {
+                _this.onArticleTagClicked(tag);
+            };
+            html.appendChild(tagNode);
+        });
+    };
+    UiHandler.findArticleChildNodeById = function (articleId) {
+        var list = IndexHtml_1["default"].articleList();
+        var children = list.children;
+        for (var i = 0; i < children.length; i++) {
+            var child = children[i];
+            var currentId = child.getAttribute(UiHandler.ATTR_ARTIFACT_ID);
+            if (currentId == articleId) {
+                return child;
+            }
+        }
+        throw new Error("Article not found by ID [" + articleId + "]!");
+    };
+    UiHandler.prototype.updateArticleNode = function (article) {
+        var _this = this;
+        var child = UiHandler.findArticleChildNodeById(article.id);
+        var articleTitleLink = child.getElementsByClassName(UiHandler.CLASS_TITLE)[0].firstChild;
+        articleTitleLink.innerHTML = article.title;
+        articleTitleLink.onclick = function () {
+            _this.onArticleTitleClicked(article);
+        };
+        this.resetTags(child.getElementsByClassName(UiHandler.CLASS_TAGS)[0], article.tags);
+        child.getElementsByClassName(UiHandler.CLASS_BODY)[0].innerHTML = article.body;
     };
     // COMMON
     // ------------========================================================------------
-    UiHandler.prototype.getInputValue = function (selector) {
+    UiHandler.switchButtonsToCreateMode = function (isCreateMode) {
+        IndexHtml_1["default"].btnCreate().hidden = !isCreateMode;
+        IndexHtml_1["default"].btnUpdate().hidden = isCreateMode;
+        IndexHtml_1["default"].btnCancel().hidden = isCreateMode;
+        IndexHtml_1["default"].btnDelete().hidden = isCreateMode;
+    };
+    // TODO use IndexHtml instead
+    UiHandler.getInputValue = function (selector) {
         return document.getElementById(selector).value;
     };
-    UiHandler.prototype.setInputValue = function (selector, newValue) {
+    UiHandler.setInputValue = function (selector, newValue) {
         document.getElementById(selector).value = newValue;
     };
+    // TODO make public and use from controller
     UiHandler.prototype.scrollToTop = function () {
         document.body.scrollTop = 0; // safari
         document.documentElement.scrollTop = 0; // chrome, firefox, IE, opera
     };
+    // HTML FACTORY
+    // ------------========================================================------------
+    UiHandler.ATTR_ARTIFACT_ID = "data-artifactId";
+    UiHandler.CLASS_TITLE = "articleTitle";
+    UiHandler.CLASS_TAGS = "articleTags";
+    UiHandler.CLASS_BODY = "articleBody";
     return UiHandler;
 }());
 exports["default"] = UiHandler;
